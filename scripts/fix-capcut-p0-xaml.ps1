0dc24cd921664b6bf473343bb8124c61725003ee
bedanzivot91-dev <bedanzivot91@gmail.com>
2026-08-20 06:26:08 +0200

    Remove temporary XAML compatibility helper

--- scripts/fix-capcut-p0-xaml.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-$path = 'src/NPVideoStudio.App/Views/WorkspaceView.axaml'
-$text = [System.IO.File]::ReadAllText((Resolve-Path $path), [System.Text.Encoding]::UTF8)
-$old = '<Grid ColumnDefinitions="*,*,*,*" ColumnSpacing="6">'
-$new = '<Grid ColumnDefinitions="*,*,*,*">'
-if (-not $text.Contains($old)) { throw 'CapCut P0 crop grid anchor not found' }
-$text = $text.Replace($old, $new)
-[System.IO.File]::WriteAllText((Resolve-Path $path), $text, (New-Object System.Text.UTF8Encoding($false)))
-Write-Host 'Removed unsupported Avalonia Grid.ColumnSpacing.'
