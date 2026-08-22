aa621d74e7c530e5cb58b93ccf9148853d8958be
bedanzivot91-dev <bedanzivot91@gmail.com>
2026-08-22 11:57:01 +0200

    feat: wire modern workspace chrome views

---
+++ src/NPVideoStudio.App/Views/ModernWorkspaceChromeViews.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+
+namespace NPVideoStudio.App.Views;
+
+public partial class ModernWorkspaceHeaderView : UserControl
+{
+    public ModernWorkspaceHeaderView() => InitializeComponent();
+}
+
+public partial class ModernWorkspaceCommandBarView : UserControl
+{
+    public ModernWorkspaceCommandBarView() => InitializeComponent();
+}
+
+public partial class ModernInspectorView : UserControl
+{
+    public ModernInspectorView() => InitializeComponent();
+}
